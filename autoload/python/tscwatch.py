d4fef64a89bd37df9809b25a2909e94f915693a8
higuri <35358263+higuri@users.noreply.github.com>
2018-04-08 18:44:44 +0900

    Parse error message

--- autoload/python/tscwatch.py
+++ autoload/python/tscwatch.py
@@ -14,7 +14,12 @@ tscwatch_thread = None
 class TscWatchThread(Thread):
 
     CMD = ['tsc', '--watch']
-    PAT = re.compile(r'Starting .*compilation.*\.\.\.')
+    PAT_START = re.compile(
+            r'\d{2}:\d{2}:\d{2} - .*Starting .*compilation.*\.\.\.')
+    PAT_END = re.compile(
+            r'\d{2}:\d{2}:\d{2} - Compilation complete\.')
+    PAT_ERROR = re.compile(
+            r'(.+)\((\d+),(\d+)\): (.*)')
 
     def __init__(self, args):
         Thread.__init__(self)
@@ -24,6 +29,7 @@ class TscWatchThread(Thread):
     def run(self):
         cmd = self.CMD + self.args
         self.proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
+        isTscFailed = False
         while True:
             try:
                 line = self.proc.stdout.readline()
@@ -32,11 +38,33 @@ class TscWatchThread(Thread):
                 s = line.decode('utf-8').strip()
                 if s == '':
                     continue
-                if self.PAT.search(s) is not None:
-                    # Clear quickfix list.
+                if self.PAT_START.search(s) is not None:
+                    isTscFailed = False
+                    # clear
                     vim.command('call setqflist([], "r")')
-                    vim.command('call setqflist([], "r", {"title": "%s"})' % ' '.join(cmd))
-                vim.command('caddexpr "%s"' % s)
+                    # set title
+                    vim.command('call setqflist([], "r", ' +
+                        '{"title": "%s"})' % ' '.join(cmd))
+                elif self.PAT_END.search(s) is not None:
+                    if isTscFailed:
+                        vim.command('copen')
+                    else:
+                        print('[OK] %s' % ' '.join(cmd))
+                else:
+                    if not isTscFailed:
+                        isTscFailed = True
+                    m = self.PAT_ERROR.search(s)
+                    if m is not None:
+                        # add error item
+                        vim.command('call setqflist([{' +
+                                '"filename": "%s",' % (m.group(1)) + 
+                                '"lnum": %s,' % (m.group(2)) +
+                                '"col": %s,' % (m.group(3)) + 
+                                '"text": "%s"' % (m.group(4)) +
+                                '}])')
+                    else:
+                        # unknown pattern
+                        vim.command('caddexpr "%s"' % s)
                 vim.command('redraw')
             except:
                 break
@@ -47,7 +75,6 @@ class TscWatchThread(Thread):
 # tscwatch_start:
 def tscwatch_start(args):
     global tscwatch_thread
-    vim.command('copen')
     if tscwatch_thread and tscwatch_thread.is_alive():
         print('Already started')
         return
@@ -57,7 +84,6 @@ def tscwatch_start(args):
 # tscwatch_stop:
 def tscwatch_stop():
     global tscwatch_thread
-    vim.command('cclose')
     if tscwatch_thread is None:
         return
     if tscwatch_thread.is_alive():
